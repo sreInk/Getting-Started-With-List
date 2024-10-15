aa7fb5eff847ebd36683822b1424e183f70a9e61
sreInk <srinknok@gmail.com>
2024-10-15 09:09:14 +0530

    w

---
+++ Word Matching.py
@@ -0,0 +1,9 @@
+def mw(words):
+    ctr = 0
+    lsr = []
+    for word in words:
+            if len(word) > 1 and word[0] ==[-1]:
+                     ctr += 1
+    print("List of ctr")
+count = mw(['abc','cfc','xyz','aba'])
+print("number are", count)
